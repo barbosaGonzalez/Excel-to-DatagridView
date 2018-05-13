4af2b8e0e2f48b02b8e97bdcd252bdc9f4214b04
Edsel Barbosa González <edselbarbosagonzalez@gmail.com>
2018-05-13 11:27:59 -0500

    Carga incial

---
+++ Importar Excel a DataGridView/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Importar_Excel_a_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.txt_Sheet = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Escoje archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cargar Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Location = new System.Drawing.Point(115, 6);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(330, 20);
+            this.txt_Path.TabIndex = 2;
+            // 
+            // txt_Sheet
+            // 
+            this.txt_Sheet.Location = new System.Drawing.Point(115, 38);
+            this.txt_Sheet.Name = "txt_Sheet";
+            this.txt_Sheet.Size = new System.Drawing.Size(330, 20);
+            this.txt_Sheet.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(607, 350);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 425);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_Sheet);
+            this.Controls.Add(this.txt_Path);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_Path;
+        private System.Windows.Forms.TextBox txt_Sheet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Importar Excel a DataGridView/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Importar_Excel_a_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if(op.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txt_Path.Text = op.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string PatCpnn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + txt_Path.Text + "; Extended Properties=\"Excel 8.0;HDR=Yes;\";";
+            OleDbConnection conn = new OleDbConnection(PatCpnn);
+            OleDbDataAdapter myDataAdapter = new OleDbDataAdapter("Select * from ["+txt_Sheet.Text+"$]", conn);
+            DataTable dt = new DataTable();
+            myDataAdapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
